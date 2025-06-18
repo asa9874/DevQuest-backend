4c7dbe1a1815d240d44854de007bdf2282024157
asa9874 <pjkpjk1129@gmail.com>
2025-06-18 21:06:07 +0900

    Feat: JWT 설정 및 로그인 구현

--- backend/src/main/java/com/devquest/domain/auth/service/AuthService.java
+++ backend/src/main/java/com/devquest/domain/auth/service/AuthService.java
@@ -7,7 +7,9 @@ import com.devquest.domain.auth.dto.requestDto.AuthLoginRequestDto;
 import com.devquest.domain.auth.dto.requestDto.AuthRegisterRequestDto;
 import com.devquest.domain.auth.dto.responseDto.AuthLoginResponseDto;
 import com.devquest.domain.member.model.Member;
+import com.devquest.domain.member.model.Role;
 import com.devquest.domain.member.repository.MemberRepository;
+import com.devquest.global.jwt.JwtTokenProvider;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     public void register(AuthRegisterRequestDto requestDto) {
         if(memberRepository.existsByEmail(requestDto.email())) {
@@ -28,11 +31,20 @@ public class AuthService {
                 .email(requestDto.email())
                 .password(passwordEncoder.encode(requestDto.password()))
                 .name(requestDto.name())
+                .role(Role.USER)
                 .build();
         memberRepository.save(member);
     }
 
     public AuthLoginResponseDto login(AuthLoginRequestDto requestDto) {
-        return new AuthLoginResponseDto();
+        Member member =memberRepository.findByEmail(requestDto.email())
+                .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
+
+        if (!passwordEncoder.matches(requestDto.password(), member.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        String token = jwtTokenProvider.createToken(member.getEmail(), member.getRole().name(), member.getId());
+        return new AuthLoginResponseDto(token);
     }
 }
--- backend/src/main/java/com/devquest/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/devquest/domain/member/repository/MemberRepository.java
@@ -1,5 +1,7 @@
 package com.devquest.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devquest.domain.member.model.Member;
@@ -7,4 +9,5 @@ import com.devquest.domain.member.model.Member;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     boolean existsByName(String name);
+    Optional<Member> findByEmail(String email);
 }

9cc565e7f254427303a9b705c4b731fe3a48ad1d
asa9874 <pjkpjk1129@gmail.com>
2025-06-19 10:16:19 +0900

    Feat: AuthLoginRequestDto 및 AuthRegisterRequestDto에 유효성 검사 추가

--- backend/src/main/java/com/devquest/domain/auth/dto/requestDto/AuthRegisterRequestDto.java
+++ backend/src/main/java/com/devquest/domain/auth/dto/requestDto/AuthRegisterRequestDto.java
@@ -5,4 +5,15 @@ public record AuthRegisterRequestDto(
         String email,
         String password
 ) {
+    public AuthRegisterRequestDto {
+        if (name == null || name.isBlank()) {
+                throw new IllegalArgumentException("이름은 비어 있을 수 없습니다.");
+        }
+        if (email == null ) {
+                throw new IllegalArgumentException("이메일이 비어 있을 수 없습니다.");
+        }
+        if (password == null || password.length() < 6) {
+                throw new IllegalArgumentException("비밀번호는 최소 6자 이상이어야 합니다.");
+        }
+    }
 }

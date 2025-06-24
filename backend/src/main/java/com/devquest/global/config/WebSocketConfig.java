9c3cb8808abaafa776cb6d14e893d55c0a6844eb
asa9874 <pjkpjk1129@gmail.com>
2025-06-24 16:20:20 +0900

    Feat: 웹소켓 설정 추가 및 JWT 핸드쉐이크 인터셉터 구현

---
+++ backend/src/main/java/com/devquest/global/config/WebSocketConfig.java
@@ -0,0 +1,36 @@
+package com.devquest.global.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import com.devquest.global.jwt.JwtHandshakeInterceptor;
+import com.devquest.global.jwt.PrincipalHandshakeHandler;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private JwtHandshakeInterceptor jwtHandshakeInterceptor;
+
+    @Override
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:5173", "http://localhost:8080", "http://127.0.0.1:3000")
+                .addInterceptors(jwtHandshakeInterceptor)
+                .setHandshakeHandler(new PrincipalHandshakeHandler())
+                .withSockJS();
+    }
+
+}
---
+++ backend/src/main/java/com/devquest/global/jwt/JwtHandshakeInterceptor.java
@@ -0,0 +1,62 @@
+package com.devquest.global.jwt;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class JwtHandshakeInterceptor implements HandshakeInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    public JwtHandshakeInterceptor(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+            WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        String token = getTokenFromQuery(request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            String email = jwtTokenProvider.getEmailFromToken(token);
+            String role = jwtTokenProvider.getRoleFromToken(token);
+            Long userId = jwtTokenProvider.getIdFromToken(token);
+
+            attributes.put("email", email);
+            attributes.put("role", role);
+            attributes.put("userId", userId);
+            return true;
+
+        }
+        log.error("No token found in query parameters");
+        return false; 
+    }
+
+    private String getTokenFromQuery(ServerHttpRequest request) {
+        String query = request.getURI().getQuery();
+        if (query != null) {
+            String[] params = query.split("&");
+            for (String param : params) {
+                if (param.startsWith("token=")) {
+                    return param.substring(6);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+            WebSocketHandler wsHandler, Exception exception) {
+    }
+}

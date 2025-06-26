44d7024cd7d0a1e1f660e5174225a10fd45b85f0
asa9874 <pjkpjk1129@gmail.com>
2025-06-26 15:59:52 +0900

    Feat: 길드 채팅방 및 메시지 모델 추가

---
+++ backend/src/main/java/com/devquest/domain/guildChat/model/GuildChatRoom.java
@@ -0,0 +1,47 @@
+package com.devquest.domain.guildChat.model;
+
+import com.devquest.domain.guild.model.Guild;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GuildChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "guild_id", nullable = false)
+    private Guild guild;
+
+    @Builder
+    public GuildChatRoom(String title, String description, Guild guild) {
+        this.title = title;
+        this.description = description;
+        this.guild = guild;
+    }
+
+    public void update(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+}
---
+++ backend/src/main/java/com/devquest/domain/guildChat/model/GuildChatRoomMessage.java
@@ -0,0 +1,50 @@
+package com.devquest.domain.guildChat.model;
+
+import java.lang.reflect.Member;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GuildChatRoomMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "guild_chat_room_id", nullable = false)
+    private GuildChatRoom guildChatRoom;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime sentAt;
+
+    @Builder
+    public GuildChatRoomMessage(GuildChatRoom guildChatRoom, Member member, String content, LocalDateTime sentAt) {
+        this.guildChatRoom = guildChatRoom;
+        this.member = member;
+        this.content = content;
+        this.sentAt = sentAt;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+}
+

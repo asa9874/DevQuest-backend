2e80e6c8aee06510ac84111058e236fd417394b9
asa9874 <pjkpjk1129@gmail.com>
2025-07-16 16:32:34 +0900

    Refact: AuthService Exception 변경

--- backend/src/main/java/com/devquest/domain/auth/service/AuthService.java
+++ backend/src/main/java/com/devquest/domain/auth/service/AuthService.java
@@ -3,6 +3,7 @@ package com.devquest.domain.auth.service;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,10 @@ import com.devquest.domain.auth.dto.responseDto.AuthLoginResponseDto;
 import com.devquest.domain.member.model.Member;
 import com.devquest.domain.member.model.Role;
 import com.devquest.domain.member.repository.MemberRepository;
+import com.devquest.global.exception.customException.DuplicateDataException;
 import com.devquest.global.jwt.JwtTokenProvider;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -27,10 +30,10 @@ public class AuthService {
 
     public void register(AuthRegisterRequestDto requestDto) {
         if(memberRepository.existsByEmail(requestDto.email())) {
-            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+            throw new DuplicateDataException("이미 존재하는 이메일입니다.");
         }
         if(memberRepository.existsByName(requestDto.name())) {
-            throw new IllegalArgumentException("이미 존재하는 이름입니다.");
+            throw new DuplicateDataException("이미 존재하는 이름입니다.");
         }
         Member member = Member.builder()
                 .email(requestDto.email())
@@ -44,7 +47,7 @@ public class AuthService {
 
     public AuthLoginResponseDto login(AuthLoginRequestDto requestDto) {
         Member member =memberRepository.findByEmail(requestDto.email())
-                .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
+                .orElseThrow(() -> new EntityNotFoundException("회원을 찾을 수 없습니다."));
 
         if (!passwordEncoder.matches(requestDto.password(), member.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
@@ -59,7 +62,7 @@ public class AuthService {
             token = token.substring(7);
         }
         if (!jwtTokenProvider.validateToken(token)) {
-            throw new RuntimeException("Token이 유효하지 않습니다.");
+            throw new AccessDeniedException("Token이 유효하지 않습니다.");
         }
 
         redisTemplate.opsForValue().set("blacklist:" + token, "blacklisted", 1, TimeUnit.HOURS); // TTL 1시간
